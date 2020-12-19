706511b2998b7457c919b164fdd7a7a09a767166
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-12-19 00:47:27 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,20 +5,20 @@
 #    else:
 #        light.clear()
 
-while True:
-    print("Light Level:" + input.light_level())
-    if input.light_level() < 6:
-        light.set_all(light.rgb(0, 0, 255))
-    elif input.light_level() > 11:
-        light.set_all(light.rgb(255, 255, 0))
-    else:
-        light.clear()
+#while True:
+#    print("Light Level:" + input.light_level())
+#    if input.light_level() < 6:
+#        light.set_all(light.rgb(0, 0, 255))
+#    elif input.light_level() > 11:
+#        light.set_all(light.rgb(255, 255, 0))
+#    else:
+#        light.clear()
 
 while True:
     print("Light Level:" + input.light_level())
     if input.light_level() < 6:
         light.set_all(light.rgb(0, 0, 255))
     elif input.light_level() > 11:
-        light.set_all(light.rgb(255, 255, 0))
+        light.set_all(light.rgb(255,69,0))
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -4,23 +4,20 @@
 //         light.set_all(light.rgb(255, 255, 255))
 //     else:
 //         light.clear()
+// while True:
+//     print("Light Level:" + input.light_level())
+//     if input.light_level() < 6:
+//         light.set_all(light.rgb(0, 0, 255))
+//     elif input.light_level() > 11:
+//         light.set_all(light.rgb(255, 255, 0))
+//     else:
+//         light.clear()
 while (true) {
     console.log("Light Level:" + input.lightLevel())
     if (input.lightLevel() < 6) {
         light.setAll(light.rgb(0, 0, 255))
     } else if (input.lightLevel() > 11) {
-        light.setAll(light.rgb(255, 255, 0))
-    } else {
-        light.clear()
-    }
-    
-}
-while (true) {
-    console.log("Light Level:" + input.lightLevel())
-    if (input.lightLevel() < 6) {
-        light.setAll(light.rgb(0, 0, 255))
-    } else if (input.lightLevel() > 11) {
-        light.setAll(light.rgb(255, 255, 0))
+        light.setAll(light.rgb(255, 69, 0))
     } else {
         light.clear()
     }
