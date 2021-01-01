dca3598ecb9db87bfe3e4ffbd432fb9e247c5cd1
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-01 17:38:20 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -14,11 +14,13 @@
 #    else:
 #        light.clear()
 
-while True:
-    print("Light Level:" + input.light_level())
-    if input.light_level() < 6:
-        light.set_all(light.rgb(0, 0, 255))
-    elif input.light_level() > 14:
-        light.set_all(light.rgb(255,69,0))
-    else:
-        light.clear()
+#while True:
+#    print("Light Level:" + input.light_level())
+#    if input.light_level() < 6:
+#        light.set_all(light.rgb(0, 0, 255))
+#    elif input.light_level() > 14:
+#        light.set_all(light.rgb(255,69,0))
+#    else:
+#        light.clear()
+
+# Light Project:
